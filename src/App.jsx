5f363656b2d08110cec8be50b95285a4f3d45378
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-04-02 13:36:06 +0530

    small components created. added in 'App'

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
+    </>
+  )
+};
+
+export default App
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+        <h2>Main Content</h2>
+        <p className='text'>I love Japan and i am planning to visit next year.</p>
+    </main>
+  )
+}
+
+export default MainContent
