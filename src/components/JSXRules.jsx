efc5f4a11a188f8ae62c5b5e61dfe866408be96d
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-04-02 18:14:42 +0530

    jsxrules learning done!

--- src/components/JSXRules.jsx
+++ src/components/JSXRules.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 
 const JSXRules = () => {
-    const courseName = 'Javascript Mastery'
-
+  const courseName = "Javascript Mastery";
+  const multiply = (a, b) => a * b
   return (
     <div>
       <p>
@@ -10,10 +9,13 @@ const JSXRules = () => {
         closed 3) JSX attributes are written in camelCase (eg: className insted
         of class)
       </p>
-      <p> This is an expression: 2 + 2 = {2+2}</p>
-      <p> The course name is : 
-        <h3>{courseName}</h3>
+      <p> This is an expression: 2 + 2 = {2 + 2}</p>
+      <p>
+        {" "}
+        The course name is : {courseName}
       </p>
+      <p>My friends List: {['Alex', 'John']}</p>
+      <p>The value of 15 * 15 = {multiply(15, 15)}</p>
     </div>
   );
 };
